1f8d5b14c5463825f75e0656709643410df81108
strotskiy0001 <strotskiy.vv@gmail.com>
2020-09-21 21:09:40 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,105 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    /* Task 5<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static void main(String[] args) {
+        int a, b, h;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input h");
+        h = keyboard.nextInt();
+        System.out.println("Input a");
+        a = keyboard.nextInt();
+        System.out.println("Input b");
+        b = keyboard.nextInt();
+        System.out.println(h/(a-b));
+    }*/
+    /*/Task 1<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int c;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input a");
+        a = keyboard.nextInt();
+        System.out.println("Input b");
+        b = keyboard.nextInt();
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a + " " +b);
+    }*/
+    /*  TASK 8 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int s;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input a");
+        a = keyboard.nextInt();
+        System.out.println("Input b");
+        b = keyboard.nextInt();
+        System.out.println("Input s");
+        s = keyboard.nextInt();
+        if (a == b && b == s)
+            System.out.println("3");
+        else if (a == b && s == a && b == s)
+        System.out.println("2");
+else if (a > b && a < s && b > s);
+            System.out.println("0");
+
+
+    }*/
+
+    /* TASK 8 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int s;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input a");
+        a = keyboard.nextInt();
+        System.out.println("Input b");
+        b = keyboard.nextInt();
+        System.out.println("Input s");
+        s = keyboard.nextInt();
+        if (a == b && b == s)
+            System.out.println(a+ " " +b +" " +s);
+        else if (a == b && b > s)
+            System.out.println(a+ " " +b);
+        else if (a > b && a < s && b > s) ;
+        System.out.println(" ");
+    }*/
+
+    /* TASK 3 <<<<<<<<<<<<<<<<<<<<<<<<<<<< no work :(
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        int x;
+        int y;
+        int z;
+        int sec = 60;
+        int min = 3600;
+
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input a");
+        a = keyboard.nextInt();
+        System.out.println("Input b");
+        c = keyboard.nextInt();
+        System.out.println("Input c");
+        c = keyboard.nextInt();
+        System.out.println("Input x");
+        x = keyboard.nextInt();
+        System.out.println("Input y");
+        y = keyboard.nextInt();
+        System.out.println("Input z");
+        z = keyboard.nextInt();
+        System.out.println((x-a)3600 + (y-b)60 + z - c);
+    }*/
+}
